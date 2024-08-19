91ef45087a8f154ec0a7f599b67fbe738de15a00
jicodes <liu.ji1@northeastern.edu>
2024-08-19 15:16:32 -0700

    Chapter 15: part 3: the drop trait

--- deref_and_drop/src/main.rs
+++ deref_and_drop/src/main.rs
@@ -17,21 +17,46 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+// Drop trait
+struct CustomSmartPointer {
+    data: String,
+}
+
+// In Rust, the Drop trait is automatically in scope
+// because it’s part of the standard prelude
+// impl Drop for CustomSmartPointer with `drop` method
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
     let x = 5;
     // let y = &x;
     let y = MyBox::new(x);
-    
+
     assert_eq!(5, x);
     assert_eq!(5, *y); // *y is equivalent to *(y.deref())
-
 
     // Deref coercion
     let m = MyBox::new(String::from("Rust"));
     hello(&m); // hello(&(*m));
-    // &MyBox<String> -> &String -> &str
+               // &MyBox<String> -> &String -> &str
 
     // DerefMut trait for mutable references
+
+    // Drop trait
+
+    // the data will be dropped in reverse order of their creation
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    drop(c); // provided by the standard library, explicitly drop a value, change the order
+    println!("CustomSmartPointers created.");
 }
 
 fn hello(name: &str) {
