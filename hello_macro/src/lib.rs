33778baeff45d56dadc8109815e05608364abcb0
jicodes <122512755+jicodes@users.noreply.github.com>
2024-09-06 15:36:05 -0700

    example crate for procedural macros

---
+++ hello_macro/src/lib.rs
@@ -0,0 +1,11 @@
+// use proc_marco;
+
+// #[some_attribute]
+// pub fn some_function(input:TokenStream) -> TokenStream {
+//     // --snip--
+// }
+
+
+pub trait HelloMacro {
+    fn hello_macro();
+}
